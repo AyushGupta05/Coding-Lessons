3b42766a639a0b5126416b23ff8bf0a08de3af3e
Nirmal <nirmalk.web@gmail.com>
2021-06-10 18:21:18 +0530

    added basic script

---
+++ Coloring/basic-lessons.sh
@@ -0,0 +1,23 @@
+# Shellscript - Comments for Human. Computer will ignore that line. It will execute it
+
+echo "Hello Ayush"
+
+#read -p "Enter the Color name" picked_color
+
+#echo "You entered Color : $picked_color " 
+
+#[1,2,3,4]
+
+colors=(red green yellow orange brown blue)
+
+for colorName in "${colors[@]}"
+do
+
+  mkdir -p colors/${colorName[@]}
+  echo  "I am ${colorName[@]}"  > colors/${colorName[@]}/open_me.txt
+  echo "${colorName[@]}"
+
+done
+
+
+
