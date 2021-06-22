6ccf97e7a157a75d7714868c192568802cb1766f
Ayush Gupta <Ayush@Saurabhs-Mac-mini.local>
2021-06-22 17:16:53 +0530

    Read the promise

---
+++ shellscript/grocery.sh
@@ -0,0 +1,11 @@
+# Ayush Grocery supermarket shop bill
+# The aim of the program is to generate a bill for the purchased grocery products
+# The user will enter what they want and how much they want and we need to calculate the total cost and show it to them
+# use read command to get the grocery name and then check what ever the user has entered 
+# if the user entered tomato then the price is 10rs
+# if the user entered potato, the price is 15rs
+# if the user entered carrot the price is 5rs
+# if the user entered apple the price is 20rs
+# if the user entered banana the price is 15rs
+# ask the user how much they want and them multiply the cost into the quantity
+
---
+++ shellscript/readme.md
@@ -0,0 +1,27 @@
+x = "apple"
+y = " apple 1, apple 2, apple 3, apple 4"
+z=2+5
+b = z*3
+d = (b * 3) + (7 * (6 / 5)) - (20 * 6)
+e = 7%4 
+f(x)= 2x + 3
+f(x) = x^2 + 2*x 
+f = function
+()denote the place to pass arguments
+x is a variable and argument to the function f
+
+f(x,y,z) = 2*x + 3*y + 4*z
+
+f = start_robot
+x = sunday
+start_robot (x)
+function start here #
+collect newspaper
+water the plants
+arrange the staple stand
+function end here
+
+start_robot ("monday")
+dayname= "wednesday"
+start_robot (dayname)
+
